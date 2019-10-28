dc29910f1e934107db2717976a350bd8a6b850cb
zhangkai <zhangkai123995@126.com>
2019-10-28 19:03:55 +0800

    components

--- src/components/Watch/index.vue
+++ src/components/Watch/index.vue
@@ -5,17 +5,22 @@
     <button @click="handleChange1">点击改变 obj.fullName.firstName</button>
     <br />
     <button @click="handleChange2">点击改变 obj.fullName.lastName</button>
+    <br />
+    <button @click="stop">stop watch</button>
+    <div v-cloak>{{ message }}</div>
   </div>
 </template>
 <script>
 let num1 = 1;
 let num2 = 1;
+let unwatch = null;
 export default {
   name: "Watch",
   info: ["实例的监听功能"],
   Zhname: "实例 watch",
   data() {
     return {
+      message: null,
       obj: {
         fullName: {
           firstName: "zhang",
@@ -31,8 +36,8 @@ export default {
   },
   watch: {
     obj: {
-      handler(n, o) {
-        console.log("deep watch");
+      handler() {
+        this.log("deep watch");
       },
       deep: true
     }
@@ -45,18 +50,32 @@ export default {
     handleChange2() {
       num2++;
       this.obj.fullName.lastName = "song" + num2;
+    },
+    stop() {
+      unwatch();
     }
   },
   created() {
-    this.$watch("obj.fullName.firstName", function(newVal, oldVal) {
+    setTimeout(() => {
+      this.message = "123455767";
+    }, 1000);
+    // this.$watch("obj.fullName.firstName", function(newVal, oldVal) {
+    //   this.log(newVal, oldVal);
+    //   this.log("firstName changed");
+    // });
+    // this.$watch("obj.fullName.lastName", function(newVal, oldVal) {
+    //   this.log("lastName changed");
+    // });
+
+    unwatch = this.$watch("obj.fullName.firstName", function(newVal, oldVal) {
+      this.log(newVal, oldVal);
       this.log("firstName changed");
     });
-    this.$watch("obj.fullName.lastName", function(newVal, oldVal) {
-      this.log("lastName changed");
-    });
-    // this.$watch("obj.fullName", function(newVal, oldVal) {
-    //   this.log("fullName changed");
-    // });
   }
 };
 </script>
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
